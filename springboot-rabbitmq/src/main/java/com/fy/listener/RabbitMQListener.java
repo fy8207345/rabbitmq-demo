d67ec98a690430c654f59f450d9529797ce596a4
fengyan <2316696377@qq.com>
2021-11-24 14:53:44 +0800

    iii

--- springboot-rabbitmq/src/main/java/com/fy/listener/RabbitMQListener.java
+++ springboot-rabbitmq/src/main/java/com/fy/listener/RabbitMQListener.java
@@ -3,8 +3,8 @@ package com.fy.listener;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.PublisherCallbackChannel;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,7 +15,7 @@ public class RabbitMQListener {
 
     @RabbitListener(queues = "boot-queue")
     public void onMessage(String msg, Channel channel, Message message) throws IOException {
-        log.info("接收到消息string：{}" , msg);
+        log.info("接收到消息string：{} - {}" , msg, message.getMessageProperties());
         //手动ack
         channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
--- springboot-rabbitmq/src/test/java/com/fy/Publisher.java
+++ springboot-rabbitmq/src/test/java/com/fy/Publisher.java
@@ -1,12 +1,20 @@
 package com.fy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.core.MessagePropertiesBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
+@Slf4j
 @SpringBootTest
 public class Publisher {
 
@@ -15,6 +23,22 @@ public class Publisher {
 
     @Test
     void contextLoads() throws IOException {
-        rabbitTemplate.convertAndSend("boot-topic-exchange", "fast.black.rabbit", "fast.black.rabbit");
+        String id = UUID.randomUUID().toString();
+        log.info("发送消息的id: {}", id);
+//        CorrelationData messageId = new CorrelationData(id);
+//        rabbitTemplate.convertAndSend("boot-topic-exchange", "fast.red.rabbit", "fast.black.rabbit", messageId);
+//        MessageProperties properties = MessagePropertiesBuilder.newInstance()
+//                .setMessageId(id)
+//                .setHeader("foo", "bar")
+//                .build();
+//        Message message = MessageBuilder.withBody("fast.red.rabbit".getBytes(StandardCharsets.UTF_8))
+//                .andProperties(properties)
+//                .build();
+        //发送meSsage对象，可以携带更多属性
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setMessageId(id);
+        messageProperties.setHeader("foo", "bar");
+        Message message = new Message("fast.red.rabbit".getBytes(StandardCharsets.UTF_8), messageProperties);
+        rabbitTemplate.convertAndSend("boot-topic-exchange", "fast.red.rabbit", message);
     }
 }
