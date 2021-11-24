c584d2210adf91453c17a743a9ac43fea7b97368
fengyan <2316696377@qq.com>
2021-11-24 15:10:56 +0800

    iii

--- springboot-rabbitmq/src/main/java/com/fy/listener/RabbitMQListener.java
+++ springboot-rabbitmq/src/main/java/com/fy/listener/RabbitMQListener.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.PublisherCallbackChannel;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,9 +15,10 @@ import java.io.IOException;
 public class RabbitMQListener {
 
     @RabbitListener(queues = "boot-queue")
-    public void onMessage(String msg, Channel channel, Message message) throws IOException {
+    public void onMessage(String msg, Channel channel, Message message, @Header(value = "foo", required = false) String foo) throws IOException {
         log.info("接收到消息string：{} - {}" , msg, message.getMessageProperties());
         //手动ack
+        System.out.println(foo);
         channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 }
